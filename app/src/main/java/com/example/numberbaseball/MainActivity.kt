2a24e3ea1780dece6bcb2ba0f2cc806b5dbc15f0
Hk4549 <hkjw4549@gmail.com>
2019-08-23 11:37:09 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.numberbaseball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        init()
+    }
+
+    fun init() {
+        val logger = Logger.getLogger(MainActivity::class.java.name)
+        val randomValue = (1..9).random()
+        logger.info("random value $randomValue")
+        textTT.text = randomValue.toString()
+
+    }
+
+}
