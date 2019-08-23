082f8897be7ef5fc5620b5017d6b9b1c0b4d6181
Hk4549 <hkjw4549@gmail.com>
2019-08-23 13:13:29 +0900

    라인 제거
    https://github.com/BeokBeok/NumberBaseBall/pull/2#discussion_r316967824

--- app/src/main/java/com/example/numberbaseball/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -6,8 +6,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
-
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
