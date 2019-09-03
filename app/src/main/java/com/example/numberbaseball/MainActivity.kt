0e5dd5f7aec7b5fed5dd7894fe60c20a6160c7cd
Hk4549 <hkjw4549@gmail.com>
2019-09-03 16:22:18 +0900

    숫자 야구때 사용할 중복이 제거된 3개의 랜덤숫자를 세팅 하는 로직 추가

--- app/src/main/java/com/example/numberbaseball/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -14,8 +14,46 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
     private fun initView() {
         // 버튼 클릭 시 숫자를 랜덤하게 보여줌
         btn_get_random_number.setOnClickListener {
-            tv_random_number.text = getRandomNumber()
+            tv_random_number.text = initBaseballNumber()
+
         }
+    }
+
+    /**
+     * 숫자 야구때 사용할 번호 세팅
+     */
+    private fun initBaseballNumber(): String {
+        val randomList = mutableListOf("-1", "-1", "-1")
+        var checkCount = 0
+
+        while (true) {
+            val randomTemp = getRandomNumber()
+            // 중복 유무 체크
+            var notOverlap = false
+
+            for (x in 0..2) {
+                if (randomList[x] == randomTemp) {
+                    notOverlap = false
+                    break
+                } else {
+                    notOverlap = true
+                }
+            }
+
+            if (notOverlap) {
+                randomList[checkCount] = randomTemp
+                checkCount++
+            }
+
+            println("랜덤 리스트값 : $randomList")
+
+            if (checkCount >= 3) {
+                break
+            }
+        }
+
+        return randomList.toString()
+
     }
 
     /**
