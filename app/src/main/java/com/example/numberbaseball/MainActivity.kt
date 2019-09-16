9829874dd70a0ba19b123cf140657b1074ad835c
Hk4549 <hkjw4549@gmail.com>
2019-09-16 16:39:15 +0900

    1. Log 메시지 추가 2. printLn 삭제 3. 램덤 숫자 불러오는 메소드 삭제

--- app/src/main/java/com/example/numberbaseball/MainActivity.kt
+++ app/src/main/java/com/example/numberbaseball/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.numberbaseball
 
 import android.os.Bundle
+import android.util.Log
 import com.example.numberbaseball.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,7 +15,7 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
     private fun initView() {
         // 버튼 클릭 시 숫자를 랜덤하게 보여줌
         btn_get_random_number.setOnClickListener {
-            tv_random_number.text = initBaseballNumber()
+            tv_random_number.text = setBaseballNumber()
 
         }
     }
@@ -22,15 +23,16 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
     /**
      * 숫자 야구때 사용할 번호 세팅
      */
-    private fun initBaseballNumber(): String {
+    private fun setBaseballNumber(): String {
 
         val numberMap = mutableMapOf<Int, Int>()
 
         while (true) {
-            val randomTemp = getRandomNumber()
+            // (n..m).random 은 1 부터 9 까지의 랜덤한 숫자를 반환
+            val randomTemp = (1..9).random()
             numberMap[randomTemp] = randomTemp
 
-            println("numberMap 데이터 : " + numberMap.values)
+            Log.d("MainActivity", "numberMap 데이터 : " + numberMap.values)
 
             if (numberMap.size == 3) {
                 break
@@ -41,10 +43,4 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
 
     }
 
-    /**
-     * Single-expression functions
-     * (n..m).random 은 n 부터 m 까지의 랜덤한 숫자를 반환
-     * 따라서 1~9 사이의 랜덤숫자를 반환
-     */
-    private fun getRandomNumber(): Int = (1..9).random()
 }
